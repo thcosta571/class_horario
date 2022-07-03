49e68acd6007b2dc6b24e18e52d0650440dadbff
thcosta571 <58777494+thcosta571@users.noreply.github.com>
2022-07-03 12:03:31 -0300

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,18 @@
+module Classe_horario {
+}import java.util.Calendar;
+
+public class Testa_Metodos_Set_Get_Calendar{
+
+	public static void main(String[] args) {
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.YEAR, 1995);
+		c.set(Calendar.MONTH, Calendar.MARCH);
+		c.set(Calendar.DAY_OF_MONTH, 20);
+
+		System.out.println("Data/Hora atual: "+c.getTime());
+		System.out.println("Ano: "+c.get(Calendar.YEAR));
+		System.out.println("Mês: "+c.get(Calendar.MONTH));
+		System.out.println("Dia do Mês: "+c.get(Calendar.DAY_OF_MONTH));
+	}
+
+}
